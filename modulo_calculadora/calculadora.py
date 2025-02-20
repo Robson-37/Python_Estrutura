1b9d2b98c97646362a17f37076330691a219d784
Robson Luiz <robson.luizsilva2019@gmail.com>
2025-02-20 20:00:00 -0300

    Atualização

--- modulo_calculadora/calculadora.py
+++ modulo_calculadora/calculadora.py
@@ -1,14 +1,14 @@
 
-def soma(a, b):
+def somar(a, b):
      return  a + b
 
 def subtrair(a, b):
     return a - b
 
-def mutiplicacao(a, b):
+def multiplicar(a, b):
     return a * b
 
-def divisao(a,b):
+def dividir(a,b):
     if b == 0:
         raise ValueError("Divisão por zero não permitida")
     return a / b
--- teste/test_calculadora.py
+++ teste/test_calculadora.py
@@ -1,20 +1,26 @@
 import unittest
-from modulo_calculadora import calculadora 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../modulo_calculadora")))
+import calculadora
 
-class TesteCalculadoda(unittest.TestCase):
+class TesteCalculadora(unittest.TestCase):
 
     def test_somar(self):
-        resultado = calculadora.soma(3,2)
+        resultado = calculadora.somar(3,2)
         self.assertEqual(resultado,6)
 
     def test_subtrair(self):
         resultado = calculadora.subtrair(3,1)
-        self.assertEqual(resultado,1)
+        self.assertEqual(resultado,2)
     
-    #def test_mutiplicar(self):
-        #self.assertEqual(mutiplicacao(3,1),3)
-    #def test_subtrair(self):
-        #self.assertEqual(divisao(3,1),3)
+    def test_multiplicar(self):
+        resultado = calculadora.multiplicar(3, 1)
+        self.assertEqual(resultado, 3)
+
+    def test_dividir(self):
+        resultado = calculadora.dividir(3, 1)
+        self.assertEqual(resultado, 3)
 
 if __name__=='__main__':
     unittest.main()
